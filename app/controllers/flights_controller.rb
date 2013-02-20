32fafa41a9ab92f0301faad29ec6febed809ab61
Arjun Venkataswamy <arjun@Arjuns-MacBook-Air.local>
2013-02-20 15:36:43 -0600

    added seeds file to create flights and made index view dynamic

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
 
   def index
-
+    @flights = Flight.order('number asc').limit(500)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Flight.destroy_all
+
+airports = [
+            ['ORD', 'JFK', 740],
+            ['ORD', 'LAX', 1744],
+            ['ORD', 'SFO', 1846],
+            ['JFK', 'LAX', 2475],
+            ['JFK', 'SFO', 2586],
+            ['LAX', 'SFO', 338]
+          ]
+
+100.times do
+  airport = airports.sample
+  randomizer = [0,1].shuffle
+  f = Flight.new
+  f.number = rand(100..999)
+  f.departure_airport = airport[randomizer[0]]
+  f.arrival_airport = airport[randomizer[1]]
+  f.miles = airport[2]
+  f.departs_at = Time.now.beginning_of_day + rand(24.hours)
+  f.seats = 60
+  f.save
+end
+
+puts "Created 100 flights"
+
+
+
+
