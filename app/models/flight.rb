75b26663f9bcb1fefadf7fe5eb6e37d27d7ef092
Jeff Cohen <jeff@codeacademy.org>
2013-02-20 17:21:00 -0600

    Finish user story #1

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,13 @@
 class Flight < ActiveRecord::Base
   attr_accessible :arrival_airport, :departs_at, :departure_airport, :miles, :number, :seats
+
+
+  def arrives_at
+    departs_at + duration.minutes
+  end
+
+  def duration
+    miles / 8
+  end
+
 end
