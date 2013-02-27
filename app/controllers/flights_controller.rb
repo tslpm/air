c3d478a3db462290fa111baa84e55ebf07104979
Jeff Cohen <jeff@codeacademy.org>
2013-02-27 15:44:31 -0600

    Fuzzy search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,8 +3,9 @@ class FlightsController < ApplicationController
   def index
     if params[:keyword].present?
       keyword = params[:keyword]
-      # @flights = Flight.where(departure_airport: params[:keyword], arrival_airport: params[:keyword])
-      @flights = Flight.where("departure_airport = ? or arrival_airport = ?", keyword, keyword)
+      @flights = Flight.where("departure_airport LIKE ? or arrival_airport LIKE ?",
+                                "%#{keyword}%",
+                                "%#{keyword}%")
       @flights = @flights.order('number asc')
     else
       @flights = Flight.order('number asc')
