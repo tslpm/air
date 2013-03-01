11067deb982fc10197259671d3c4d69309e9f0e6
Raghu Betina <rbetina@gmail.com>
2013-03-01 14:30:10 -0600

    Got pagination working. Trying to do previous and next links.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -11,6 +11,22 @@ class FlightsController < ApplicationController
       @flights = Flight.order('number asc')
     end
     @flights = @flights.limit(500)
+
+    if params[:page].present?
+      offset_number = (params[:page].to_i - 1) * 10
+      @flights = @flights.offset(offset_number)
+    end
+
+    @flights = @flights.limit(10)
+
+    @num_pages = (Flight.count / 10.0).ceil
+
   end
 
 end
+
+
+
+
+
+
