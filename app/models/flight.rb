22442fddf7da70b648ccab0523c4db611a486530
Jeff Cohen <jeff@codeacademy.org>
2013-03-15 14:25:01 -0500

    Tests for the Flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,26 @@
 class Flight < ActiveRecord::Base
   attr_accessible :arrival_airport, :departs_at, :departure_airport, :miles, :number, :seats
 
+  before_create :calculates_miles
+
+  MILEAGE_CHART = [
+            ['ORD', 'JFK', 740],
+            ['ORD', 'LAX', 1744],
+            ['ORD', 'SFO', 1846],
+            ['JFK', 'LAX', 2475],
+            ['JFK', 'SFO', 2586],
+            ['LAX', 'SFO', 338]
+          ]
+
+  def calculates_miles
+    e = MILEAGE_CHART.detect do |entry|
+      (entry.first == self.departure_airport &&
+            entry.second == self.arrival_airport) ||
+      (entry.second == self.departure_airport &&
+            entry.first == self.arrival_airport)
+    end
+    self.miles = e.third
+  end
 
   def arrives_at
     departs_at + duration.minutes
--- test/unit/flight_test.rb
+++ test/unit/flight_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class FlightTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a flight calculates its own miles" do
+    f = Flight.new
+    f.departure_airport = 'ORD'
+    f.arrival_airport = 'JFK'
+    f.save
+
+    assert_equal 740, f.miles
+  end
+
+  test "a flight from LAX to SFO" do
+    f = Flight.new
+    f.departure_airport = "LAX"
+    f.arrival_airport = "SFO"
+    f.save
+
+    assert_equal 338, f.miles
+  end
+  test "a flight from LAX to ORD" do
+    f = Flight.new
+    f.departure_airport = "LAX"
+    f.arrival_airport = "ORD"
+    f.save
+
+    assert_equal 1744, f.miles
+  end
 end
